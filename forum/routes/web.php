8de0ec744a24b55442df697bfc4e3f11993be829
Paul Marques Mota <paul@marquesmota.pt>
2018-04-24 23:22:18 +0200

    add route for /show

--- forum/routes/web.php
+++ forum/routes/web.php
@@ -2,4 +2,5 @@
 Route::get('/', 'ForumController@index');
 Route::get('/create', 'ForumController@create');
 Route::post('/store/', 'ForumController@store');
+Route::post('/show', 'ForumController@show');
 
