407ce84fd5e668e8f367f2ed743b1730941ca27f
Paul Marques Mota <paul@marquesmota.pt>
2018-05-03 21:04:29 +0200

    add create and edit routes

--- forum/routes/web.php
+++ forum/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 Route::get('/', 'ForumController@index');
 Route::get('/create', 'ForumController@create');
+Route::get('/create', 'ForumController@create');
 Route::post('/store', 'ForumController@store');
 Route::post('/show', 'ForumController@show');
+Route::put('/edit', 'ForumController@edit');
 Route::put('/update', 'ForumController@update');
 Route::delete('/destroy', 'ForumController@destroy');
 
